c37a4138ae89b7ba2d786ebc8db478a3c95f36ec
Diana <diana.lania@interia.pl>
2023-06-05 22:50:47 +0200

    Creating if statement age check

---
+++ src/Ifstatement/Testingif.java
@@ -0,0 +1,18 @@
+package Ifstatement;
+
+import java.util.Scanner;
+
+public class Testingif {
+    public static void main (String [] args){
+    Scanner input = new Scanner(System.in);
+    int age;
+    System.out.println("Please enter your age:");
+    age = input.nextInt();
+    if (age > 13) {
+        System.out.println("You are eligible to register to the website.");
+        System.out.println("You can register also you using the mobile app.");
+    }
+        else
+        System.out.println("Sorry, you are too young to register to our website.");
+    }
+}
